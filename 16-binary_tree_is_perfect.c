7c71dc1e6e487e963c128439a597ff2c6945e80b
RimJoudi <joudirim93@gmail.com>
2021-07-20 23:49:19 +0100

    betty fixed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -4,18 +4,18 @@
  * @tree: a pointer to the root node of the tree to traverse
  * Return: the height of a binary tree
  */
- size_t binary_tree_height(const binary_tree_t *tree)
- {
-     size_t h_left, h_right;
-    
-    if (tree == NULL)
-        return (0);
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	 size_t h_left, h_right;
 
-    h_left = binary_tree_height(tree->left);
-    h_right = binary_tree_height(tree->right);
-    if (h_left >= h_right)
-        return (h_left + 1);
-    return (h_right + 1);
+	if (tree == NULL)
+		return (0);
+
+	h_left = binary_tree_height(tree->left);
+	h_right = binary_tree_height(tree->right);
+	if (h_left >= h_right)
+		return (h_left + 1);
+	return (h_right + 1);
 }
 
 /**
@@ -24,21 +24,18 @@
  * Return: 1 when binary tree is perfect otherwise 0
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
-{  
-    if (tree == NULL)
-        return 0;
-
-    // Check the presence of children
-    if (tree->left == NULL && tree->right == NULL)
-        return 1;
-
-    if (tree->left == NULL || tree->right == NULL)
-        return 0;
-
-   if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+{
+	if (tree == NULL)
+		return (0);
+	/*Check the presence of children*/
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
 	{
 		if (binary_tree_is_perfect(tree->left) &&
-			binary_tree_is_perfect(tree->right))
+		binary_tree_is_perfect(tree->right))
 			return (1);
 	}
 	return (0);
