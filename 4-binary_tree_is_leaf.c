eb28db4a7c033345dc3103192455ad1cb407f7dd
RimJoudi <joudirim93@gmail.com>
2021-07-20 23:16:21 +0100

    betty fixed

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,10 +6,10 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return 0;
-    
-    if (node->right == NULL && node->left == NULL)
-        return 1;
-    return 0;
+	if (node == NULL)
+		return (0);
+
+	if (node->right == NULL && node->left == NULL)
+		return (1);
+	return (0);
 }
