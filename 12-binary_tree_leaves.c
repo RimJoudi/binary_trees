e2dc23d7bbd2c624bd7a878ac058436badeada3d
RimJoudi <77971241+RimJoudi@users.noreply.github.com>
2021-07-20 23:06:10 +0100

    commit to task12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: a pointer to the root node of the tree to count the number of leaves
+ * Return: number of leaves in a binary tree otherwise 0
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return 0;
+    
+    if (tree->left == NULL && tree->right == NULL)
+        return 1;
+    
+    return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+}
