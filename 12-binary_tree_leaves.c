f1056de635dc02048cabf41acd3023fa490dc059
RimJoudi <joudirim93@gmail.com>
2021-07-20 23:40:30 +0100

    betty fixed

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,11 +6,11 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
-    
-    if (tree->left == NULL && tree->right == NULL)
-        return 1;
-    
-    return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
