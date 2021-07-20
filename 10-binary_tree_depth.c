2b968dee32c4226da7c1ac7fdad4a293a3cc0ead
RimJoudi <joudirim93@gmail.com>
2021-07-20 23:23:10 +0100

    betty fixed

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -5,17 +5,14 @@
  * Return: the depth of a node in a binary tree
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
-{   
-    size_t Depth;
-    if (tree == NULL)
-        return 0;
+{
+		size_t Depth;
 
-    if (tree->parent == NULL)
-		return (0);
-   
-
-    Depth = binary_tree_depth (tree->parent);
-    Depth++;
-    
+		if (tree == NULL)
+			return (0);
+		if (tree->parent == NULL)
+			return (0);
+		Depth = binary_tree_depth(tree->parent);
+		Depth++;
 	return (Depth);
 }
