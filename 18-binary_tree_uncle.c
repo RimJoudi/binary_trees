32c8d4afb2ea776e1514b21499b3942a921382ca
RimJoudi <77971241+RimJoudi@users.noreply.github.com>
2021-07-21 18:50:45 +0100

    commit to task18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: a pointer to the node to find the uncle
+ * Return: a pointer to the uncle node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+   binary_tree_t *parent;
+
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	parent = node->parent;
+	if (parent->parent->left == parent)
+		return (parent->parent->right);
+	else
+		return (parent->parent->left); 
+
+}
