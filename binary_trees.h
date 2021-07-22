d46220d4b55c8e4c8883aec6fcd19258088a15de
RimJoudi <joudirim93@gmail.com>
2021-07-22 22:38:21 +0100

    update header file

--- binary_trees.h
+++ binary_trees.h
@@ -43,4 +43,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+int binary_tree_complete(const binary_tree_t *tree, size_t index, size_t number_nodes);
+int binary_tree_is_complete(const binary_tree_t *tree);
 #endif /* _BINARY_TREES_H_ */
